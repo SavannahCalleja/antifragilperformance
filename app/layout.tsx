530167f9a63e6af8aec360b3204242acb03c5cb0
Savannah Calleja <savannahcalleja@macbookpro.mynetworksettings.com>
2026-04-04 11:59:44 -0400

    added app welcome screen

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { InstallGuide } from "./install-guide";
 import { SiteNav } from "./site-nav";
+import { WelcomeScreen } from "./welcome-screen";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -42,6 +43,7 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="flex min-h-full flex-col">
+        <WelcomeScreen />
         <SiteNav />
         {children}
         <InstallGuide />
